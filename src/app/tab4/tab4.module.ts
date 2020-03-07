04c7eaa7f51212d78bfa85a9f769e28f3ab43780
Rengko Panusunan Malau <49031581+renkomalau@users.noreply.github.com>
2020-03-07 15:34:12 +0700

    update routing

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
-import { Tab4PageRoutingModule } from './tab4-routing.module';
-
 import { Tab4Page } from './tab4.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab4PageRoutingModule
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: Tab4Page }])
   ],
   declarations: [Tab4Page]
 })
