4e79187a669bf6241a6cbb63caaa0cfc726ff8b0
Rengko Panusunan Malau <49031581+renkomalau@users.noreply.github.com>
2020-03-18 16:11:04 +0700

    deploy android

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -18,61 +18,61 @@ export class Tab1Page {
     this.pages = [
       {
         title: 'Diabetes',
-        thumb: './assets/images/a.jpeg',
+        thumb: './assets/images/diabetes.png',
         description: 'Who we are. What we do. Why we’re here',
         link: "/about"
       },
       {
         title: 'Corona',
-        thumb: './assets/images/b.jpeg',
+        thumb: './assets/images/corona.png',
         description: 'Drop us a line and get in touch we’d love to hear from you!',
         link: "/contact"
       },
       {
         title: 'Anemia',
-        thumb: './assets/images/c.jpeg',
+        thumb: './assets/images/anemia.png',
         description: 'Third party resources used by this app',
         link: "/technology"
       },
       {
         title: 'Depresi',
-        thumb: 'assets/images/d.jpeg',
+        thumb: 'assets/images/depresi.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
         title: 'Tuberkulosis',
-        thumb: 'assets/images/e.jpg',
+        thumb: 'assets/images/batuk.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
         title: 'HIV/AIDS',
-        thumb: 'assets/images/f.jpg',
+        thumb: 'assets/images/hiv.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
       title: 'Malaria',
-        thumb: 'assets/images/g.jpg',
+        thumb: 'assets/images/malaria.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
         title: 'Rubella',
-        thumb: 'assets/images/h.jpg',
+        thumb: 'assets/images/rabella.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
-        title: 'DBD',
-        thumb: 'assets/images/i.png',
+        title: 'Insomnia',
+        thumb: 'assets/images/insomnia.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       },
       {
         title: 'Rabies',
-        thumb: 'assets/images/j.jpg',
+        thumb: 'assets/images/rabies.png',
         description: 'The latest news updates from our Twitter account',
         link: "TweetsPage"
       }
@@ -82,56 +82,56 @@ export class Tab1Page {
       {
         id : '1',
         name : 'Dr.Ade Senorita, Sp.PD',
-        description : 'Dokter Spesialis Penyakit Dalam',
+        description : 'Spesialis Penyakit Dalam',
         link: "bnbcnas",
         image: 'assets/images/doctor/1.jpeg'
       },
       {
         id : '2',
-        name : 'Dr.April Hidayat, Sp.B,FINACS, FICS',
-        description : 'Dokter Spesialis Bedah',
+        name : 'Dr.April Hidayat, Sp.B',
+        description : 'Spesialis Bedah',
         link: "bnbcnas",
         image: 'assets/images/doctor/2.jpg'
       },
       {
         id : '3',
         name : 'Dr.Lindayanti,Sp.PK',
-        description : 'Dokter Spesialis Kesehatan Jiwa',
+        description : 'Spesialis Kesehatan Jiwa',
         link: "bnbcnas",
         image: 'assets/images/doctor/3.jpg'
       },
       {
         id : '4',
         name : 'Dr.Zalmah',
-        description : 'Dokter Umum',
+        description : 'Umum',
         link: "bnbcnas",
         image: 'assets/images/doctor/4.jpg'
       },
       {
         id : '5',
         name : 'Dr.Jamar Hasan, Sp.A',
-        description : 'Dokter Spesialis Anak',
+        description : 'Spesialis Anak',
         link: "bnbcnas",
         image: 'assets/images/doctor/5.jpg'
       },
       {
         id : '6',
         name : 'Dr.Antonius Sianturi, Sp.P',
-        description : 'Dokter Spesialis Paru',
+        description : 'Spesialis Paru',
         link: "bnbcnas",
         image: 'assets/images/doctor/6.jpg'
       },
       {
         id : '7',
         name : 'Dr.Ali Akbar, Sp.P',
-        description : 'Dokter Spesialis Mata',
+        description : 'Spesialis Mata',
         link: "bnbcnas",
         image: 'assets/images/doctor/7.jpg'
       },
       {
         id : '8',
         name : 'Dr.Muhammad Wijaya, Sp.P',
-        description : 'Dokter Spesialis Kulit',
+        description : 'Spesialis Kulit',
         link: "bnbcnas",
         image: 'assets/images/doctor/8.jpg'
       },
