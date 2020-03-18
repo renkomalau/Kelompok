13c73212c7f49efe613b13cb82dbee9444b2f08b
Rengko Panusunan Malau <49031581+renkomalau@users.noreply.github.com>
2020-03-18 17:34:21 +0700

    tab chat

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -5,9 +5,49 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
 
-  constructor() { }
+
+
+export class Tab4Page implements OnInit {
+  contacts: Array <{
+    contactName: string,
+    contactAvatar: string,
+    snippet: string,
+    time: Date}>;
+
+
+  constructor() { 
+    this.contacts= [
+        {
+          contactName: "Finn",
+          contactAvatar:"https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-finn.png",
+          snippet: "Listen, I've had a pretty messed up day...",
+          time: new Date(Date.now())
+        },
+        {
+          contactName: "Han",
+          contactAvatar:
+            "https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-han.png",
+          snippet: "I've got enough on my plate as it is, and I...",
+          time: new Date(Date.now())
+        },
+        {
+          contactName: "Rey",
+          contactAvatar:
+            "https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-rey.png",
+          snippet: "You will remove these restraints and leave...",
+          time: new Date(Date.now())
+        },
+        {
+          contactName: "Luke",
+          contactAvatar:
+            "https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-luke.png",
+          snippet: "I feel the good in you, the conflict...",
+          time: new Date(Date.now())
+        }
+      ];
+    }
+  
 
   ngOnInit() {
   }
